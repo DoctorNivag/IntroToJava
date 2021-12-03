5d354a5ab1425cb8a0146e4afb7146e63a6c1968
doctornivag <doctornivag@penguin.com>
2021-12-02 23:28:13 -0800

    tired var let and const but probably failed

---
+++ script.js
@@ -0,0 +1,10 @@
+var numb1 = 604;
+var numb2 = 56;
+let numb3 = 72;
+const numb4 = 471;
+
+function addition(numb1, numb1, numb2){
+    console.log(numb1 + numb1 + numb2);
+}
+
+addition(numb3, numb1);
